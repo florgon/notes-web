8aa1156feba186f35040968b50e6aa9d97c05fdb
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-03-04 18:28:59 +0400

    Rename project to `notes_web`  && `notes` app.

--- src/notes_web/settings.py
+++ src/notes_web/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'notes.urls'
+ROOT_URLCONF = 'notes_web.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'notes.wsgi.application'
+WSGI_APPLICATION = 'notes_web.wsgi.application'
 
 
 # Database
