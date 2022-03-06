8c5df181581851765185c4a8401928b47890477c
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-03-06 23:16:37 +0400

    Note modified date, button redesign

--- src/frontend/src/components/Note.js
+++ src/frontend/src/components/Note.js
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
-const Note = function ({onDeleteNote, id, text, created_at}){
+const Note = function ({onDeleteNote, id, text, created_at, updated_at}){
+  const convertDate = function(timestamp){
+    return new Date(timestamp).toLocaleDateString("en-GB", {
+      "hour": "2-digit",
+      "minute": "2-digit"
+    })
+  }
+  const dateIsSame = function(timestamp_a, timestamp_b){
+    /// Date is same when difference in minutes is less than 1.
+    let diff = (new Date(timestamp_a).getTime() - new Date(timestamp_b).getTime()) / 60000;
+    return Math.abs(diff) < 1
+  }
   return (
     <div className="__note__ card shadow">
       <div className="card-header">
         <div className="__note__id display-6 text-center">
           Note #{id} 
         </div>
+        <div className="__note__created__at__ col text-muted text-center">
+          Created at {convertDate(created_at)}
+        </div>
+        
+        <div className="__note__modified__at__ col text-muted text-center">
+          {dateIsSame(created_at, updated_at) && 
+            <Fragment>Was not changed</Fragment>
+          }
+          {!dateIsSame(created_at, updated_at) &&
+            <Fragment>Updated at {convertDate(updated_at)}</Fragment>
+          }
+        </div>
       </div>
       <div className="card-body">
         <div className="__note__text__ display-6">
           {text}
         </div>
-        <div className="row border-top">
+      </div>
+      <div className="card-footer">
+      <div className="row">
           <div className="col">
-            <div className="__note__created__at__ col text-muted">
-              Created at {new Date(created_at).toLocaleDateString("en-GB", {
-                "hour": "2-digit",
-                "minute": "2-digit"
-              })}
-            </div>
-          </div>
-          <div className="col">
-            <a onClick={(e) => {
-              onDeleteNote(id, e);
-            }}>delete</a>
-          </div>
+          <button className="btn btn-danger" onClick={(e) => {
+                onDeleteNote(id, e);
+              }}>Delete
+          </button></div>
         </div>
       </div>
     </div>
