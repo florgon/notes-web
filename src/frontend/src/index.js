86a33bb5f83f44e3a4e6b9d1866aa1549408de58
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-03-07 01:08:08 +0400

    Includes

--- src/frontend/src/index.js
+++ src/frontend/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+import './i18nextConf';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
