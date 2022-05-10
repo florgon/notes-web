71047ec8fdb886e380adb9b08b01424442f8a7b6
Kirill Zhosul <79853674+kirillzhosul@users.noreply.github.com>
2022-05-10 22:58:51 +0400

    Update footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,32 +1,16 @@
 // Libraries.
 import React from 'react';
-import {useTranslation} from 'react-i18next';
-import {Link} from 'react-router-dom';
-
-const FooterButtons = function(){
-    /// @description Buttons on the footer.
-    const {t} = useTranslation();
-    return (
-        <div className="mx-auto row">
-            <span className="fw-bold text-muted col">
-                {t("built-with-love")}<a href="https://kirillzhosul.site" className="link-info">{t("author-name-by")}</a>.
-            </span>
-            <span className="text-muted col">
-                <a href="https://github.com/florgon/notes-web" className="text-reset" target="_blank" rel="noreferrer">{t("source-code")}</a>.
-            </span>
-            <span className="text-muted col">
-                <Link to="/dev/docs" className="text-reset">{t("for-developers")}</Link>.
-            </span>
-        </div>
-    )
-}
 
 const Footer = function() {
     /// @description Page footer.
     return (
         <footer className="bg-light mt-auto">
           <div className="text-center p-2 border-top">
-            <FooterButtons/>
+            <div className="mx-auto row">
+                <div className="col-lg mb-1">
+                    &copy; 2022 <a href="https://florgon.space">Florgon</a>. <i>{"<"}support@florgon.space{">"}</i>
+                </div>
+            </div>
           </div>
         </footer>
     )
